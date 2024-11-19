7e2afc5ecef62f828143f0399f7b684f6dc19152
tchu2803 <tchu280305@gmail.com>
2024-11-19 17:11:52 +0700

    .

--- view/sanphamct.php
+++ view/sanphamct.php
@@ -2,47 +2,53 @@
     <div class="boxtrai mr" style="margin-bottom: auto;">
 
         <div class="row mb">
-            <?php
-            if (isset($_SESSION['user'])) {
-            ?>
-                <div class="boxtitle"><?= $onesp['name'] ?></div>
+            <?php if (isset($onesp)): ?>
+                <div class="boxtitle"><?= htmlspecialchars($onesp['name']) ?></div>
                 <div class="row boxcontent">
-                    <?= $onesp['mota']; ?>
+                    <?= nl2br(htmlspecialchars($onesp['mota'])) ?>
                     <div class="row mb">
-                        <img class="spct" src="<?= BASE_URL . $onesp['img'] ?>">
+                        <img class="spct" src="<?= htmlspecialchars(BASE_URL . $onesp['img']) ?>" alt="Hình ảnh sản phẩm">
                     </div>
                 </div>
+            <?php else: ?>
+                <p>Không tìm thấy thông tin sản phẩm.</p>
+            <?php endif; ?>
         </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-        <script>
-            $(document).ready(function() {
-                $("#binhluan").load("view/binhluan/binhluanform.php", {
-                    idpro: <?= $onesp['id'] ?>
+
+        <?php if (isset($_SESSION['user'])): ?>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+            <script>
+                $(document).ready(function() {
+                    const idpro = <?= isset($onesp['id']) ? json_encode($onesp['id']) : 'null' ?>;
+                    if (idpro) {
+                        $("#binhluan").load("view/binhluan/binhluanform.php", { idpro: idpro });
+                    }
                 });
-            });
-        </script>
-    <?php } else { ?>
-        <div class="boxtitle"><?= $onesp['name'] ?></div>
-        <div class="row boxcontent">
-            <?= $onesp['mota']; ?>
-            <div class="row mb">
-                <img class="spct" src="<?= BASE_URL . $onesp['img'] ?>">
-            </div>
-        </div>
-    <?php } ?>
-    <div class="row" id="binhluan">
+            </script>
+        <?php endif; ?>
+
+        <div class="row" id="binhluan"></div>
 
         <div class="row mb">
             <div class="boxtitle">Sản phẩm cùng loại</div>
             <div class="row boxcontent">
-                <?php
-                foreach ($sp_cung_loai as $sanPham) { ?>
-                    <li><a href="index.php?act=sanphamct&idsp=<?= $sanPham['id'] ?>"><?= $sanPham['name'] ?></a></li>
-                <?php } ?>
+                <?php if (!empty($sp_cung_loai)): ?>
+                    <ul>
+                        <?php foreach ($sp_cung_loai as $sanPham): ?>
+                            <li>
+                                <a href="index.php?act=sanphamct&idsp=<?= htmlspecialchars($sanPham['id']) ?>">
+                                    <?= htmlspecialchars($sanPham['name']) ?>
+                                </a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php else: ?>
+                    <p>Không có sản phẩm cùng loại.</p>
+                <?php endif; ?>
             </div>
         </div>
     </div>
-    </div>
+
     <div class="boxphai" style="margin-bottom: auto;">
         <?php include "boxright.php"; ?>
     </div>
